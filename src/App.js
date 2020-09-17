bb714f6b2edb2b9b05a07892b6df09ee5dde35df
codewithirene567 <irenejinscott@gmail.com>
2020-09-16 22:46:50 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
   }
 
   render() {
-    // debugger;
+     debugger;
     return (
       <div className="App">
           <button onClick={() => this.handleOnClickItems()}>
@@ -34,8 +34,16 @@ class App extends Component {
 }
 
 const mapStateToProps = (state) => {
-  debugger;
+  //debugger;
   return { items: state.items }
+  //*again* the state of the items is being returned here no matter what you call it
+  //which each change to the store's state the mapStateToProps method is executed
+  //specifies which slice of the store's state
 }
 
-export default connect(mapStateToProps)(App);
+//export default connect(mapStateToProps)(App);
+export default connect( mapStateToProps) (App);
+//redux is the state part which is in mapStateToProps and react is the App part
+//connect is connecting them together
+//whatever function we pass to the connect() function will be the state of the store
+//this means line 38 is going to return the state
